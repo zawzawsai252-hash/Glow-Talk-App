f35e8e24cb158e7bde35aee2c1672846a0971e8c
zawzawsai252-hash <zawzawsai252@gmail.com>
2026-09-17 22:03:24 +0630

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
-main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
